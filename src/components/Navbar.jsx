b196a6455b587fee1ab4503cc656edd03b7b0e44
khbmh (Mahi) <khaledbinmonowarhossain@gmail.com>
2025-06-21 18:11:50 +0600

    first c

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+"use client"
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import React from 'react';
+
+const Navbar = () => {
+  const path = usePathname();
+  if (!path.includes('dashboard')) {
+    return (
+      <div className="flex w-1/2 *:cursor-pointer justify-between mx-auto my-4">
+        <Link href={'/'}>Home</Link>
+        <Link href={'/services'}>Services</Link>
+        <Link href={'/about'}>About</Link>
+      </div>
+    );
+  } else {
+    <></>;
+  }
+};
+
+export default Navbar;
